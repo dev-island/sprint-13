f171cf29848095c0da2016147372af3f1dc5f488
Andrew Locke <andrew.locke@spiral.us>
2023-12-12 19:11:58 -1000

    tuesday

--- React/intro-to-react/src/App.jsx
+++ React/intro-to-react/src/App.jsx
@@ -1,20 +1,48 @@
 import './App.css'
+import { useState } from 'react';
 
-function MyComponent() {
+const Title = ({ name }) => {
   return (
-    <div>A component</div>
+    <h1>{name}</h1>
+  )
+}
+
+const SubTitle = ({ title }) => {
+  return (
+    <h3>{title}</h3>
+  )
+}
+
+function Header({ name }) {
+  return (
+    <div>
+      <Title name={name} />
+      <SubTitle title={name} />
+    </div>
+  )
+}
+
+function Form({ name }) {
+  return (
+    <div>
+      <Header name={name}/>
+    </div>
   )
 }
 
 function App() {
+  const [count, setCount] = useState(0)
+  const name = "Andrew"
   return (
     <>
       <div>
-        Hello React
+        Current Count is {count}
       </div>
-      <MyComponent />
+      <Form name={name} />
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   )
 }
 
 export default App
+ 
